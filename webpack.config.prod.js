db113053c3cdc39fcc79ef3c68c02707728d7d04
ido <idok@wix.com>
2015-11-17 13:14:47 +0200

    add loaders

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -29,6 +29,12 @@ module.exports = {
       test: /\.js$/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
-    }]
+    },
+      {test: /\.less$/, loader: 'style-loader!css-loader!less-loader'}, // use ! to chain loaders
+      {test: /\.css$/, loader: 'style-loader!css-loader'},
+      {test: /\.scss$/, loader: 'style!css!sass'},
+      {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}, // inline base64 URLs for <=8k images, direct URLs for the rest
+      {test: /\.rt/, loaders: ['react-templates-loader?targetVersion=0.14.0'], include: path.join(__dirname, 'src')}
+    ]
   }
 };
